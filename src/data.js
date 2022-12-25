45f57553091a609be5ae547225069eba14f548f0
yujin kim <yujin@eazel.net>
2022-12-25 23:40:27 +0900

    url commit

--- src/data.js
+++ src/data.js
@@ -3,12 +3,12 @@ const url = "https://cemmanuelp.github.io/communication-equipment-heritage";
 export const datas = [
   {
     id: 1,
-    src: `${process.env.PUBLIC_URL}/images/heritage/20180906-001-blauw.jpeg`,
+    src: `/communication-equipment-heritage/images/heritage/20180906-001-blauw.jpeg`,
     imageUrlFormat: "20180906-0xx-blauw.jpeg",
   },
   {
     id: 2,
-    src: `${process.env.PUBLIC_URL}/images/heritage/20180906-001-blauw.jpeg`,
+    src: `/communication-equipment-heritage/images/heritage/20180906-001-blauw.jpeg`,
     imageUrlFormat: "20180906-0xx-blauw.jpeg",
   },
 ];
